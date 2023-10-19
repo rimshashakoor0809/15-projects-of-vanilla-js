e9bdb02b77fbdd39763735c01b93d5e0298ab553
Rimsha <rimshashakoor0809@gmail.com>
2023-10-19 14:43:28 +0500

    added timer functionality to it.

--- speed-typing-game/script.js
+++ speed-typing-game/script.js
@@ -1,22 +1,20 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 const displayQuoteElement = document.getElementById("quote-display");
 const quoteInputElement = document.getElementById("quote-input");
+const timerElement = document.getElementById('timer')
 
-const generateRandomQuote = () => {
-  return fetch(RANDOM_QUOTE_API_URL)
-    .then(res => res.json())
-    .then(data => data.content)
-}
 
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = displayQuoteElement.querySelectorAll('span');
   const arrayValue = quoteInputElement.value.split('');
 
+  
   let correct = true;
   arrayQuote.forEach((characterSpan, index) => {
 
     const inputCharacter = arrayValue[index];
-    if (inputCharacter == null) {
+    console.log("Check Input character:", inputCharacter)
+    if (inputCharacter == '') {
       characterSpan.classList.remove('correct')
       characterSpan.classList.remove('incorrect')
       correct = false
@@ -38,19 +36,37 @@ quoteInputElement.addEventListener('input', () => {
   }
 })
 
+const generateRandomQuote = () => {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(res => res.json())
+    .then(data => data.content)
+}
+
+
 async function getNextQuote() {
   const quote = await generateRandomQuote()
   displayQuoteElement.innerHTML = '';
   quote.split('').forEach(character => {
-    console.log("Character:", character)
+    // console.log("Character:", character)
     const characterSpan = document.createElement("span");
-    characterSpan.classList.add('incorrect')
     characterSpan.innerText = character
     displayQuoteElement.appendChild(characterSpan)
+    startTimer()
   });
 
 }
+let startTime; 
 
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timerElement.innerText = getTimerTime()
+  }, 1000);
+}
 
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000 )
+}
 
 getNextQuote()
